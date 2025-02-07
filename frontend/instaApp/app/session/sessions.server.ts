8e7db3bec5da64ee45406b4fca6eeb01f309d7cb
kevinle22 <kevin.le.2200@gmail.com>
2025-02-06 22:25:13 -0600

    added domain to session server

--- frontend/instaApp/app/session/sessions.server.ts
+++ frontend/instaApp/app/session/sessions.server.ts
@@ -14,16 +14,15 @@ const { getSession, commitSession, destroySession } =
         {
             // a Cookie from `createCookie` or the CookieOptions to create one
             cookie: {
-                name: "__session",
+                name: "instako__session",
 
                 // all of these are optional
-                // domain: "reactrouter.com",
+                domain: "instako-kevin.netlify.app",
                 // Expires can also be set (although maxAge overrides it when used in combination).
                 // Note that this method is NOT recommended as `new Date` creates only one date on each server deployment, not a dynamic date in the future!
                 //
                 // expires: new Date(Date.now() + 60_000),
-                httpOnly: true,
-                // maxAge: 60,
+                maxAge: 60 * 60 * 24,  // Cookie expires after 1 day
                 path: "/",
                 sameSite: "lax",
                 secrets: ["s3cret1"],
